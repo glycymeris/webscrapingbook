0a0aeb315c26d1fc35ead14f37eefb06e2e8c8e1
jesusyesares <web@jesusyesares.com>
2018-07-28 19:18:13 +0200

    First Commit
    # Conflicts:
    #	download_page.py

--- iteration_Crawler.py
+++ iteration_Crawler.py
@@ -10,10 +10,10 @@ for page in itertools.count(1):
     url = 'http://example.webscraping.com/view/-%d' % page
     html = download(url)
     if html is None:
-        #received an error trying to download this webpage
+        # received an error trying to download this webpage
         num_errors += 1
         if num_errors == max_errors:
-            #reached maximum number of consecutive errors so exit
+            # reached maximum number of consecutive errors so exit
             break
     else:
         # success - can srape the result
