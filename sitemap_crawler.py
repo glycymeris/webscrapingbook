419ec199f151e50f4671f58aa71aceb2a5050f2f
jesusyesares <web@jesusyesares.com>
2018-07-28 10:42:46 +0200

    Version traida del ejercicio download. Sin modificar.

---
+++ sitemap_crawler.py
@@ -0,0 +1,20 @@
+import urllib2
+
+
+def download(url, user_agent='wswp', num_retries=2):
+    print 'Downloading:', url
+    headers = {'User-agent': user_agent}
+    request = urllib2.Request(url, headers=headers)
+    try:
+        html = urllib2.urlopen(request).read()
+    except urllib2.URLError as e:
+        print 'Download error:', e.reason
+        html = None
+        if num_retries > 0:
+            if hasattr(e, 'code') and 500 <= e.code < 600:
+                # recursively retry 5xx HTTP errors
+                return download(url, user_agent, num_retries-1)
+    return html
+
+
+download('http://httpstat.us/500')
