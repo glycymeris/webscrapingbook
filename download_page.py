26c7ae95dfe064599b3cac6f3116b43c736795f1
jesusyesares <web@jesusyesares.com>
2018-07-28 10:36:59 +0200

    Configuramos el user-agent por defecto, que lo hemos establecido como wswp (Web Scraping with Python)

--- download_page.py
+++ download_page.py
@@ -1,17 +1,19 @@
 import urllib2
 
 
-def download(url, num_retries=2):
+def download(url, user_agent='wswp', num_retries=2):
     print 'Downloading:', url
+    headers = {'User-agent': user_agent}
+    request = urllib2.Request(url, headers=headers)
     try:
-        html = urllib2.urlopen(url).read()
+        html = urllib2.urlopen(request).read()
     except urllib2.URLError as e:
         print 'Download error:', e.reason
         html = None
         if num_retries > 0:
             if hasattr(e, 'code') and 500 <= e.code < 600:
                 # recursively retry 5xx HTTP errors
-                return download(url, num_retries-1)
+                return download(url, user_agent, num_retries-1)
     return html
 
 
