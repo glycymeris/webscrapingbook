ca9c666f60fbc3e064bde79611d265d901848507
jesusyesares <web@jesusyesares.com>
2018-07-28 18:58:17 +0200

    Modificación realizada en el crawler para limitar el número máximo de errores que puede recibir

--- iteration_Crawler.py
+++ iteration_Crawler.py
@@ -1,11 +1,20 @@
 import itertools
 
 
+# maximum number of consecutive download errors allowed
+max_errors = 5
+# current number of consecutive download errors
+num_errors = 0
+
 for page in itertools.count(1):
     url = 'http://example.webscraping.com/view/-%d' % page
     html = download(url)
     if html is None:
-        break
+        #received an error trying to download this webpage
+        num_errors += 1
+        if num_errors == max_errors:
+            #reached maximum number of consecutive errors so exit
+            break
     else:
         # success - can srape the result
-        pass
+        num_errors = 0
